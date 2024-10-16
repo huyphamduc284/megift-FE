9223decbed380b934be8ddc102ae7d8e4c7c9c6f
phamduchuy <huypd284@gmail.com>
2024-10-16 11:08:48 +0700

    fix shop page

--- client/src/main-component/ShopPage/index.js
+++ client/src/main-component/ShopPage/index.js
@@ -6,17 +6,18 @@ import Footer from "../../components/footer";
 import Scrollbar from "../../components/scrollbar";
 import FilterSidebar from "../../components/FilterSidebar";
 import FilterAllProduct from "../../components/FilterAllProduct";
-import api from "../../api";
+import api from "../../api"; // Giả sử `api()` trả về danh sách sản phẩm từ dữ liệu của bạn
 import { addToCart, addToWishList } from "../../store/actions/action";
 
 const ShopPage = ({ addToCart, addToWishList }) => {
-  const productsArray = api();
+  const productsArray = api(); // Lấy dữ liệu sản phẩm
 
   const [filter, setFilter] = useState({
     price: "",
     size: "",
     color: "",
     brand: "",
+    stock: "",
   });
 
   const priceChangeHandler = ({ target: { name, value } }) => {
@@ -28,36 +29,40 @@ const ShopPage = ({ addToCart, addToWishList }) => {
     setFilter({ ...filter, [name]: value });
   };
 
-  const priceFIlter = (price) => {
-    if (filter.price === "") {
-      return true;
-    } else if (filter.price.max && filter.price.min) {
+  const priceFilter = (price) => {
+    if (filter.price === "") return true;
+    if (filter.price.max && filter.price.min) {
       return price <= filter.price.max && price >= filter.price.min;
-    } else if (filter.price.min) {
-      return price >= filter.price.min;
-    } else {
-      return false;
     }
+    if (filter.price.min) return price >= filter.price.min;
+    return false;
+  };
+
+  const stockFilter = (stock) => {
+    return filter.stock ? stock === filter.stock : true;
   };
 
   const addToCartProduct = (product) => {
     addToCart(product, 1, filter.color, filter.size);
   };
 
+  const addToWishListProduct = (product) => {
+    addToWishList(product);
+  };
+
   const products = productsArray
-    .filter((el) => priceFIlter(el.price))
+    .filter((el) =>
+      priceFilter(parseFloat(el.price.replace(" VND", "").replace(".", "")))
+    )
     .filter((el) => (filter.size ? el.size === filter.size : true))
     .filter((el) => (filter.color ? el.color === filter.color : true))
-    .filter((el) => (filter.brand ? el.brand === filter.brand : true));
-
-  const addToWishListProduct = (products) => {
-    addToWishList(products);
-  };
+    .filter((el) => (filter.brand ? el.brand === filter.brand : true))
+    .filter((el) => stockFilter(el.stock));
 
   return (
     <Fragment>
       <Navbar hClass={"header-style-2"} />
-      <PageTitle pageTitle={"Shop"} pagesub={"Shop"} />
+      <PageTitle pageTitle={"Cửa hàng"} pagesub={"Cửa hàng"} />
       <div className="shop-section">
         <div className="container">
           <div className="row">
